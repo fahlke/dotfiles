856616373e303618b15affb39a3280a0e5876471
Alexander Fahlke <alexander.fahlke@gfk.com>
2017-09-05 12:10:33 +0200

    changed history settings

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -1,7 +1,7 @@
+# all zsh history related settings can be found in setopt.zsh in section "History"
 HISTSIZE=10000
 SAVEHIST=9000
 HISTFILE=$HOME/.zsh_history
-
 
 if [[ $DOTFILES_DEBUG -eq 1 ]]; then
     echo "DEBUG: sourced history.zsh"
--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -20,7 +20,6 @@ setopt hist_find_no_dups # When searching history don't display results already 
 setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
 setopt hist_verify # don't execute, just expand history
 setopt share_history # imports new commands and appends typed commands to history
-#setopt hist_ignore_space # remove command line from history list when first character on the line is a space
 
 # Completion
 setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
