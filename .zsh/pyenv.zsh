19b06a2150f524043d0c2314d077001a2b76d428
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2017-09-07 14:19:45 +0200

    .zsh/pyenv.zsh

--- .zsh/pyenv.zsh
+++ .zsh/pyenv.zsh
@@ -8,6 +8,16 @@ if [[ ! -f "$HOME/.pyenv/plugins/pyenv-virtualenv/bin/pyenv-virtualenv" ]]; then
     git clone https://github.com/pyenv/pyenv-virtualenv.git "$HOME/.pyenv/plugins/pyenv-virtualenv"
 fi
 
+# https://github.com/pyenv/pyenv-update
+if [[ ! -f "$HOME/.pyenv/plugins/pyenv-update/bin/pyenv-update" ]]; then
+    git clone git://github.com/pyenv/pyenv-update.git "$HOME/.pyenv/plugins/pyenv-update"
+fi
+
+# https://github.com/pyenv/pyenv-doctor
+if [[ ! -f "$HOME/.pyenv/plugins/pyenv-doctor/bin/pyenv-doctor" ]]; then
+    git clone git://github.com/yyuu/pyenv-doctor.git "$HOME/.pyenv/plugins/pyenv-doctor"
+fi
+
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
